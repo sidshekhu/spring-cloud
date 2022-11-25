bdf2c57f49ee8cb19c3b48d026a30e6e7cc9f444
Shadab Siddique <shadabsiddique@Shadabs-MacBook-Pro.local>
2022-11-25 18:32:36 +0530

    config server

---
+++ spring-config-cloud-server/src/main/java/com/shadabspringcloud/springconfigcloudserver/Test.java
@@ -0,0 +1,22 @@
+package com.shadabspringcloud.springconfigcloudserver;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		 ArrayList<Integer> arr = new ArrayList<>();
+//		 
+//		 arr.ensureCapacity(6);
+//		 
+//		 arr.add(9);
+//		 arr.set(0, 6);
+		
+		int val = -10%7;
+		
+		System.out.println(" val "+val);	 	 
+	}
+
+}
---
+++ spring-config-cloud-server/src/test/java/com/shadabspringcloud/springconfigcloudserver/SpringConfigCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadabspringcloud.springconfigcloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
